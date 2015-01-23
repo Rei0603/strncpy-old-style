605d25af61fcd1af5aeb7b1a9fe1ddf4e07099d7
Rei0603 <ethereal.wind@me.com>
2015-01-24 02:41:58 +0900

    Update strncpy.c

--- strncpy.c
+++ strncpy.c
@@ -3,8 +3,10 @@ char *strncpy(char *dst,const char *src,size_t n)
     size_t i;
     char *p = dst;
     /* コピー本体の古い書き方例 */
-    for(i=0; (i<n)&&(*src); i++,*dst++ = *src++);
+    for(i=0; (i<n)&&(*src); i++){
+        *dst++ = *src++;
+    }
     /* 0埋め */ 
-    for(; (i<n); i++,*dst++ = '\0');
+    for(;i<n; i++){*dst++ = 0;}
     return(p);
 }
